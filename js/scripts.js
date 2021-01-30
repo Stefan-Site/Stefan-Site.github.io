bc5e7a21edeb4ee96d186bed44782f6a606d64d7
Stefan-Site <78078857+Stefan-Site@users.noreply.github.com>
2021-01-30 12:02:18 +0100

    bilder
    bilder

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,42 @@
 /*!
     * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
     * Copyright 2013-2020 Start Bootstrap
+    
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
-    (function ($) {
+    $(function () {
+
+    var $container = $('#container').masonry({
+        itemSelector: '.item',
+        columnWidth: 200
+    });
+
+    // reveal initial images
+    $container.masonryImagesReveal($('#images').find('.item'));
+});
+
+$.fn.masonryImagesReveal = function ($items) {
+    var msnry = this.data('masonry');
+    var itemSelector = msnry.options.itemSelector;
+    // hide by default
+    $items.hide();
+    // append to container
+    this.append($items);
+    $items.imagesLoaded().progress(function (imgLoad, image) {
+        // get item
+        // image is imagesLoaded class, not <img>, <img> is image.img
+        var $item = $(image.img).parents(itemSelector);
+        // un-hide item
+        $item.show();
+        // masonry does its thing
+        msnry.appended($item);
+    });
+
+    return this;
+};    
+
+
+(function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
